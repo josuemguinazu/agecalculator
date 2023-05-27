10cdb6a14ed79fa5842aef2d889c59dca43ed320
Josué M. Guiñazú <josuemguinazu@gmail.com>
2023-05-27 20:07:09 -0300

    redesign colors - responsive view

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const resetBtn = document.querySelector('#resetBtn');
 let bday = document.querySelector('#bday');
 
 const calculateAge = () => {
-  console.log(typeof bday.value); //string
+  // console.log(typeof bday.value); //string
   let now = new Date();
   let birth = new Date(bday.value);
-  console.log(typeof birth); //object
+  // console.log(typeof birth); //object
   let ageMs = now.getTime() - birth.getTime();
   let ageSecs = Math.floor(ageMs / 1000);
   let ageMins = Math.floor(ageSecs / 60);
@@ -21,15 +21,17 @@ const calculateAge = () => {
   let ageMonths = Math.floor(ageDays / 30.4375);
   let ageYears = Math.floor(ageMonths / 12);
 
-  console.log(`years:${ageYears}, months: ${ageMonths}, days: ${ageDays}, minutes: ${ageMins}`);
+  // console.log(`years:${ageYears}, months: ${ageMonths}, days: ${ageDays}, minutes: ${ageMins}`);
   years.innerHTML = ageYears;
-  months.innerHTML = ageMonths;
-  days.innerHTML = ageDays;
-  hours.innerHTML = ageHours;
-  minutes.innerHTML = ageMins;
+  months.innerHTML = new Intl.NumberFormat('de-DE').format(ageMonths);
+  days.innerHTML = new Intl.NumberFormat('de-DE').format(ageDays);
+  hours.innerHTML = new Intl.NumberFormat('de-DE').format(ageHours);
+  minutes.innerHTML = new Intl.NumberFormat('de-DE').format(ageMins);
 };
 
-calculateBtn.addEventListener('click', calculateAge);
+calculateBtn.addEventListener('click', () => {
+  isNaN(bday.value) ? calculateAge() : alert('Please, type a valid date');
+});
 
 const reset = () => {
   bday.value = '';
